e65662df61b3402596e2e79f83c9540362370f26
joyz0 <1214327383@qq.com>
2025-04-15 10:09:46 +0000

    feat: add database package

---
+++ packages/database/seed.ts
@@ -0,0 +1,34 @@
+import prisma from './src/prisma';
+
+async function main() {
+  const response = await Promise.all([
+    // prisma.user.upsert({
+    //   where: { email: 'rauchg@vercel.com' },
+    //   update: {},
+    //   create: {
+    //     name: 'Guillermo Rauch',
+    //     email: 'rauchg@vercel.com',
+    //     image:
+    //       'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
+    //   },
+    // }),
+    prisma.user.findMany({
+      where: {
+        email: {
+          contains: 'alice@prisma.io',
+        },
+      },
+      cacheStrategy: { ttl: 60 },
+    }),
+  ]);
+  console.log(111, response);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
